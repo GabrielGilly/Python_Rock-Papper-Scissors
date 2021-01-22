55eeef8d422256e1f8bed61b21263ff14c03a629
GabrielGilly <gabriel@gilly.st>
2021-01-22 22:34:15 +0000

    replay

--- readme.md
+++ readme.md
@@ -4,7 +4,11 @@ This game will use basic python command line interface.
 ## Game high level code
 The code will be written with the following steps in mind:
 1. welcome message and explaination of to play the first run
+create the message into a list
+print the message
 2. first run
+input from player
+print player_resu
 3. result
 4. do you want to play again ?
 5. log result
--- rock_papper_scissor.py
+++ rock_papper_scissor.py
@@ -1,44 +1,54 @@
 # 1. welcome message and explaination of to play the first run
-welcome_message=["Welcome to the rock papper scissor game."]
-welcome_message.append('+---------------------------------+')
-welcome_message.append("To play enter the letter of your choice:")
-item=['Rock','Papper','Scissors','Rock']
-welcome_message.append("0 = Rock")
-welcome_message.append("1 = Papper")
-welcome_message.append("2 = scissor")
-welcome_message.append("+---------------------------------+")
-welcome_message.append("You make the first move, what is your choice ?")
-
-for i in welcome_message:
+welcome_message="Welcome to the rock papper scissor game."
+rules=["To play enter the number of your choice:"]
+rules.append("0 = Rock")
+rules.append("1 = Papper")
+rules.append("2 = scissor")
+line="+---------------------------------+"
+print(line)
+print(welcome_message)
+for i in rules:
     print(i)
+print(line)
+def play() -> set:
+    # 2. first run
+    print("You make the first move, what is your choice ?")
+    item=['Rock','Papper','Scissors','Rock']
+    player=int(input())
+    player_result=item[player]
+    print("You choose",player_result,'.')
+    import random
+    computer=random.randint(0,2)
+    computer_result=item[computer]
+    print("The computer choose",computer_result,'.')
+    print(line)
+    #3. result
+    if item[player+1]==computer_result:
+            victory='computer'
+            message='You lost !'
+    elif player_result==computer_result:
+            victory='stand'
+            message='Nobody won.'
+    elif item[computer+1]==player_result:
+            victory='player'
+            message='You won!'
+    print()
+    print('        ',message)
+    print()
+    print(line)
 
-# 2. first run
-player=int(input())
-player_result=item[player]
-print("You choose",player_result)
-
-import random
-computer=random.randint(0,2)
-computer_result=item[computer]
-print("The computer choose",computer_result)
-print("+---------------------------------+")
-#3. result
-if item[player+1]==computer_result:
-    victory=-1
-elif player_result==computer_result:
-    victory=0
-elif item[computer+1]==player_result:
-    victory=1
-
-if victory==1:
-    print("You won!")
-elif victory==-1:
-    print("You lost!")
-elif victory==0:
-    print("no victors")
-
+play()
 
 #4. do you want to play again ?
+i=1
+while i ==1:
+    print("Do you want to play again ?")
+    print("Press ENTER key to play again or type no + press ENTER.")
+    again=str(input())
+    if again=='no':
+        i=0
+    else:
+        play()
 #5. log result
 #6. create statistics
 #7. display statistics
